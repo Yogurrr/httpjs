422f1634394ce7fffe8c701853017c0ff10e7e7b
LeeSeungYeon <masakisqq@naver.com>
2023-02-15 14:38:50 +0900

    express - 개별 라우팅 처리

---
+++ routes/about.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const html = 'text/html; charset=utf-8';
+
+// show about page
+router.get('/',(req, res) => {
+    res.type(html);
+    res.end('<h1>about 페이지입니다!!</h1>');
+});
+
+// 이 파일이 모듈로 작동하는 것을 나타냄
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const html = 'text/html; charset=utf-8';
+
+// show user page
+router.get('/',(req, res) => {
+    res.type(html);
+    res.end('<h1>user 페이지입니다!!</h1>');
+});
+
+router.get('/add',(req, res) => {
+    res.type(html);
+    res.end('<h1>user 가입 페이지입니다!!</h1>');
+});
+
+router.get('/view',(req, res) => {
+    res.type(html);
+    res.end('<h1>user 상세정보 페이지입니다!!</h1>');
+});
+
+// 이 파일이 모듈로 작동하는 것을 나타냄
+module.exports = router;
