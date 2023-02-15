b21e426919916317bf9b4e76dbd99790b320758e
LeeSeungYeon <masakisqq@naver.com>
2023-02-15 15:48:56 +0900

    express - static, sendFile, morgan

--- routes/about.js
+++ routes/about.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const path = require("path");
 const router = express.Router();
 
 const html = 'text/html; charset=utf-8';
 
 // show about page
 router.get('/',(req, res) => {
-    res.type(html);
-    res.end('<h1>about 페이지입니다!!</h1>');
+    res.sendFile(path.join(__dirname, '../public', 'about.html'));
 });
 
 // 이 파일이 모듈로 작동하는 것을 나타냄
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 const html = 'text/html; charset=utf-8';
 
 // show index page
 router.get('/',(req, res) => {
-    res.type(html);
-    res.end('<h1>index 페이지입니다!!</h1>');
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
+
+// 단순한 그림 파일을 화면에 표시하기 위해
+// 일일이 라우팅 설정하는 것은 번거로움
+/*router.get('/bird.jpg',(req, res) => {
+    // 응답으로 지정판 파일의 내용을 전송함
+    res.sendFile(path.join(__dirname, '../static/img', 'bird.jpg'));
+});*/
 
 // 이 파일이 모듈로 작동하는 것을 나타냄
 module.exports = router;
