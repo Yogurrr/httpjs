75031939156cdefd8695cbf8fa0c30ddea8c9d6e
LeeSeungYeon <masakisqq@naver.com>
2023-02-17 12:09:45 +0900

    성적 처리 2 - 모델 객체 생성 후 성적처리

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const oracledb = require('oracledb');
-const dbconfig = require('../dbconfig');
+const SungJuk = require('../models/SungJuk')
 
 const router = express.Router();
 
@@ -18,7 +17,7 @@ router.get('/sungjuk',(req, res) => {
     res.render('sungjuk', {title: '성적 처리'});
 });
 
-router.post('/sungjuk',async (req, res, next) => {
+router.post('/sungjuk',(req, res, next) => {
     // 폼으로 전송된 데이터들은 req.body, req.body.폼이름 등으로 확인 가능
     // console.log(req.body);
     // console.log(req.body.name, req.body.kor, req.body.eng, req.body.mat);
@@ -40,27 +39,8 @@ router.post('/sungjuk',async (req, res, next) => {
 
     // 데이터베이스 처리 - sungjuk 테이블에 insert
     // 한 번 만든 다음에 다시 실행하면 이미 존재하고 있어서 오류 뜸
-    let conn = null;
-    let sql = 'insert into sungjuk (sjno, name, kor, eng, mat, tot, avg, grd) values (sjno.nextval, :1, :2, :3, :4, :5, :6, :7)';
-    let params = [name, kor, eng, mat, tot, avg, grd];
-    try {
-        oracledb.initOracleClient({libDir: 'c:/Java/instantclient_19_17'});
-        conn = await oracledb.getConnection(dbconfig);
-        let result = await conn.execute(sql, params);
-        await conn.commit();
-        console.log(result);
-    } catch (e) {
-        console.log(e);
-    } finally {
-        if (conn) {
-            try {
-                await conn.close();
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    }
-
+    new SungJuk(name, kor, eng, mat, tot, avg, grd).insert();
+    // sj.insert()
 
     res.redirect(304, '/');
 });
@@ -68,7 +48,7 @@ router.post('/sungjuk',async (req, res, next) => {
 // 단순한 그림 파일을 화면에 표시하기 위해
 // 일일이 라우팅 설정하는 것은 번거로움
 /*router.get('/bird.jpg',(req, res) => {
-    // 응답으로 지정판 파일의 내용을 전송함
+    // 응답으로 지정한 파일의 내용을 전송함
     res.sendFile(path.join(__dirname, '../static/img', 'bird.jpg'));
 });*/
 
